2b44abc91d8989f4b5afa03c1b9491989a94845a
Carla Ricciardi <carla.ricciardi95@gmail.com>
2023-03-10 16:22:09 -0300

    desafio16

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 200,
+};
+
+// - arrowParens: 'always' => el arrow function siempre con parentesis
+
+// -   singleQuote: true => trabajar con comilla simple
+
+// -   trailingComma: 'all' => pone siempre la coma
+
+// -  printWidth: 200 => la linea no se caiga con tan poco texto (te mantiene todo en la misma linea hasta 200)
